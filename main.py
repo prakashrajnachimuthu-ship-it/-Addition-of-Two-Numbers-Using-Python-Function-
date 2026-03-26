44061ef6bc11377179a861d0cf90f8f925f7e81d
prakashrajnachimuthu-ship-it <prakashrajnachimuthu@gmail.com>
2026-03-26 11:42:17 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,2 @@
+def add_numbers(a, b): 
+    return a + b num1 = int(input("Enter first number: ")) num2 = int(input("Enter second number: ")) result = add_numbers(num1, num2) print("sum :", result)   
